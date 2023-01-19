0d6f75c09c310cac690363a9503d8e73dcaec53b
petrkanapelka <kanapelka-petr@mail.ru>
2023-01-19 15:24:54 +0300

    feat:created a function towesort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+    if (matrix===undefined){return []}
+    return matrix.map((el,indx)=>indx%2!==0?el.reverse():el).flat();
+};
+
